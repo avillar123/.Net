b18c690d40bba32f078004ef41eb8798ef0f75de
adolfo <avillar123@gmail.com>
2016-09-15 12:47:38 +0200

    v1.0.0

---
+++ EncryptDecrypt.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.Web.Configuration;
+
+namespace YourWebAppExample.Security
+{
+    public static class EncryptDecrypt
+    {
+        #Encrypt
+		public static void EncryptConnString()
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+            ConfigurationSection section = config.GetSection("connectionStrings");
+
+            if (!section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+                config.Save();
+            }
+        }
+
+        public static void EncryptAppSettings()
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+            ConfigurationSection section = config.GetSection("appSettings");
+
+            if (!section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+                config.Save();
+            }
+        }
+		#endif
+		
+		#Decrypt
+        public static void DecryptConnString()
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+            ConfigurationSection section = config.GetSection("connectionStrings");
+            if (section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+        }
+
+        public static void DecryptAppSettings()
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+            ConfigurationSection section = config.GetSection("appSettings");
+            if (section.SectionInformation.IsProtected)
+            {
+                section.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+        }
+		#endif
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Optimization;
+using YourWebAppExample.Security;
+
+namespace YourWebAppExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+			//When is not debug call Encrypting and Decrypting Web.config key(s)
+			#if !DEBUG
+				EncryptDecrypt.EncryptConnString(); //Encrypting connectionStrings
+				EncryptDecrypt.EncryptAppSettings(); //Encrypting appSettings
+			#endif
+			
+            //...
+        }
+    }
+}
